e038477082ed93ae84cf922e70902e167ce21eda
KangDue <rkdrlgks321@gmail.com>
2022-08-19 09:07:13 +0900

    seat change

---
+++ algorithm/0819/swea_5204/solve.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('input.txt')
+
+count = 0
+def merge_sort(x):
+    if len(x) == 1:
+        return x
+    else:
+        left = x[:len(x)//2]
+        right = x[len(x)//2:]
+        return
+
+def merge(a,b):
+
+
+for t in range(1,int(input())+1):
+    n = int(input())
+    nums = list(map(int,input().split()))
