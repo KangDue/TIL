9593521037dfad93f40120b431f7ac2786ba8aa9
부울경_1반_강기한 <rkdrlgks321@naver.com>
2022-10-06 00:42:35 +0900

    temp

--- algorithm/q.py
+++ algorithm/q.py
@@ -1,4 +1,22 @@
-for a in range(100):
-    for b in range(100):
-        print(max(a-1,b-1).bit_length())
-        print(((a-1)^(b-1)).bit_length())
+c=0
+def sep(start,end,h=0):
+    global minv, pivot,a,b
+    if h > pivot:
+        return -1
+    if start<= a <= end and start<= b <= end:
+        minv = min(minv,pivot-h)
+    sep(start,end//2,h+1)
+    sep(end//2+1,end,h+1)
+
+for a in range(1,11):
+    for b in range(1,11):
+        N = 1<<(max(a,b)).bit_length()
+        pivot = (N - 1).bit_length()
+        minv = (N - 1).bit_length()
+        sep(1, N, h=0)
+        t1 = minv
+        t2 = ((a-1)^(b-1)).bit_length()
+        print(minv,t2)
+        if t1!=t2:
+            c +=1
+print(c)
